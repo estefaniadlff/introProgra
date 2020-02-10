ef5707a0d04d11bd94a2ccac220c01a920c6f269
estefaniadlff <56362049+estefaniadlff@users.noreply.github.com>
2020-02-10 14:52:31 -0600

    radio

---
+++ radio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>radio</title>
+</head>
+<body>
+
+
+<?php 
+$radio=10;
+$n=3.1416;
+$altura=20;
+
+$area=2($pi)($altura)($radio)
+$volumen=($pi)($radio)($altura)
+
+</body>
+</html>DOSTYPE5 <HTML></HTML>
+?>
